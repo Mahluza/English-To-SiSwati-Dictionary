1485476b6c72762cee6081b317d8d15e1d9cb323
Mac User <macuser@MacintoshUsers-MacBook-2.local>
2021-03-04 23:01:24 -0700

    new push

--- server.js
+++ server.js
@@ -52,9 +52,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // pass client build to server
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
-  });
+  // app.get('*', (req, res) => {
+  //   res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+  // });
 }
 
 // apis
